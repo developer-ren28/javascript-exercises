ec8445e23e43493180c1781c7f861f83421b218b
Ren <developer.ren28@gmail.com>
2023-02-05 11:54:39 +0800

    Create reversing string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,18 @@
-const reverseString = function() {
+const reverseString = function(input) {
+
+    let originalArray = input.split("");
+    let reversedArray = input.split("");
+    let j = 0;
+  
+    for (i = originalArray.length-1; i >= 0; i--) {
+  
+      reversedArray[j] = originalArray[i];
+  
+      j++;
+        
+    };
+  
+    return reversedArray.join("");
 
 };
 
