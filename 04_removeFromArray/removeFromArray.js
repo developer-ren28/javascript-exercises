f7b6db28f9169cc9d1278f62a9331a4ddb1ea3ed
Ren <developer.ren28@gmail.com>
2023-02-06 05:40:19 +0800

    Create function removing arguement from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+
+    let i = 0;
+    
+    do {
+        
+        let remove = toRemove[i];
+
+        for(j = 0; j < array.length; j++) {
+        
+            if (array[j] === remove) {
+                array.splice(j, 1);
+            };
+        };
+
+        i++;
+
+    } while (i < toRemove.length);
+
+    return array;
+
 
 };
 
