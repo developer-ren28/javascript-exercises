b9bc82583e1512cbc2447e7713827a820d3e269c
Ren <developer.ren28@gmail.com>
2023-02-05 08:47:16 +0800

    Create repeating string function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    let word = "";
 
+    for (i = 1; i <= num; i++) {
+        word += string;
+    };
+
+    return word;
 };
 
 // Do not edit below this line
