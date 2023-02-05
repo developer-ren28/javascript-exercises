4bc41dff86b4d382502c70556cdbb810454b727b
Ren <developer.ren28@gmail.com>
2023-02-06 06:48:17 +0800

    Create sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(...numArray) {
+
+    let sum = numArray[1];
+    let firstIndex = numArray[0];
+    let secondIndex = numArray[1];
+
+    while(secondIndex !== firstIndex) {
+
+        sum += firstIndex;
+        firstIndex++;
+        
+    }; 
+
+    return sum;
 
 };
 
