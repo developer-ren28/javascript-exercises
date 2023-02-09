5256505bf67ce07e52a38934acd84552845693d0
Ren <developer.ren28@gmail.com>
2023-02-09 08:59:04 +0800

    Create Fahrenheit to Celsius and vice versa function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Ftemp) {
+
+  return celsius = Math.round(((Ftemp - 32) * (5 / 9)) * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Ctemp) {
+
+  return celsius = Math.round(((Ctemp * (5 / 9) + 32) ) * 10) / 10;
+  
 };
 
 // Do not edit below this line
