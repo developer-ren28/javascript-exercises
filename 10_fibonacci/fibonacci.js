9de6b51fcc034e75a5ddf901c3d3b6189c87cfd5
Ren <developer.ren28@gmail.com>
2023-02-12 22:50:54 +0800

    Convert the input to integer and check if it is negative integer

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,17 @@
 const fibonacci = function(nthMember) {
+    
+    let member = +nthMember;
+
+    if (nthMember < 0) {
+        return 'OOPS';
+    };
+  
     let num = 1;
     let precedingNum = [1, 1];
     let sum = 1;
     let j = 0;
 
-    for (i = 1; i < nthMember; i++) {
+    for (i = 1; i < member; i++) {
 
         sum += num;
         precedingNum.push(sum);
