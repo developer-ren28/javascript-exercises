2610aeaebb4e16fe369b1cef541186f57451f06a
Ren <developer.ren28@gmail.com>
2023-02-12 22:31:37 +0800

    Create fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(nthMember) {
+    let num = 1;
+    let precedingNum = [1, 1];
+    let sum = 1;
+    let j = 0;
 
+    for (i = 1; i < nthMember; i++) {
+
+        sum += num;
+        precedingNum.push(sum);
+        j++;
+        num = precedingNum[j];
+        
+    };
+
+    return num;
 };
 
 // Do not edit below this line
