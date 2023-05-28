e312fea4b54e8fdb91b0868a3cfbf522787f5e03
Ren <developer.ren28@gmail.com>
2023-05-29 01:42:11 +0800

    Update the solutions to use Array methods instead

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,25 +7,11 @@ const subtract = function(minuend, subtrahend) {
 };
 
 const sum = function(array) {
-	let total = 0;
-
-  for (let i = 0; i < array.length; i++) {
-    total += array[i];
-  };
-
-  return total;
+  return array.reduce((total, num) => (total + num), 0);
 };
 
 const multiply = function([...numbers]) {
-  
-  let product = 1;
-
-  for(let i = 0; i < numbers.length; i++) {
-    product *= numbers[i];
-  };
-
-  return product;
-
+  return [...numbers].reduce((total, num) => (total * num), 1);
 };
 
 const power = function(base, exponent) {
@@ -33,21 +19,18 @@ const power = function(base, exponent) {
 };
 
 const factorial = function(num) {
-
   if (num === 1 || num === 0 ) {
     return 1;
   } else if (num > 1) {
     let i = num;
   
     while (i > 1) {
-  
       num *= (i - 1)
       i--
     };
   
     return num;
   };
-
 };
 
 // Do not edit below this line
