08e8199978a7d127f74017f7e179ca05d5f3b10a
Ren <developer.ren28@gmail.com>
2023-05-30 01:55:38 +0800

    Provide solution to the exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const date = new Date();
+    const ordered = arr.sort((a, b) => {
+        const previousPerson = (a.yearOfDeath || date.getFullYear()) - a.yearOfBirth;
+        const nextPerson =  (b.yearOfDeath || date.getFullYear()) - b.yearOfBirth;
+        return previousPerson > nextPerson ? -1 : 1;
+    })
+    return ordered[0];
 };
 
 // Do not edit below this line
